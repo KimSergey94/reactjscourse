1e7d91801f538c33472f91c9ea517e5299a10bce
KimSergey94 <sksss060213@gmail.com>
2022-03-11 12:40:59 +0600

    9.5 Практическая работа 1

--- src/shared/CardsList/Card/Card.tsx
+++ src/shared/CardsList/Card/Card.tsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { displayTypeContext } from "../../context/displayTypeContext";
 import styles from './card.less';
 import { Controls, IControlsProps } from "./Controls/Controls";
-import { IMenuProps, Menu } from "./Menu/Menu";
+import { ICardMenuOpened, IMenuProps, Menu } from "./Menu/Menu";
 import { IPreviewProps, Preview } from "./Preview/Preview";
 import { ITextContentProps, TextContent } from "./TextContent/TextContent";
 
@@ -12,6 +12,7 @@ interface ICardProps{
     // menuProps: IMenuProps;
     controls: IControlsProps;
     cardId: number;
+    cards?: ICardMenuOpened;
 }
 export function Card(props: ICardProps){
     const displayType = useContext(displayTypeContext);
@@ -20,7 +21,7 @@ export function Card(props: ICardProps){
         <li className={styles.card}>
             <TextContent displayName={props.content.displayName} postedTimeAgo={props.content.postedTimeAgo} title={props.content.title} imgLink={props.content.imgLink} />
             <Preview imgSrc={props.preview.imgSrc}/>
-            <Menu displayType={displayType.displayType} cardId={props.cardId} />
+            <Menu displayType={displayType.displayType} cardId={props.cardId} cards={props.cards}/>
             <Controls karmaValue={props.controls.karmaValue} commentsNumber={props.controls.commentsNumber} />
         </li>
     );
--- src/shared/CardsList/Card/Menu/Menu.tsx
+++ src/shared/CardsList/Card/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {  TDisplayType } from '../../../context/displayTypeContext';
 import { Dropdown } from '../../../Dropdown';
 import { EIconName, Icon } from '../../../Icons/Icon';
@@ -7,8 +7,9 @@ import styles from './menu.less';
 export interface IMenuProps{
     displayType: TDisplayType;
     cardId: number;
+    cards?: ICardMenuOpened;
 }
-interface ICardMenuOpened{
+export interface ICardMenuOpened{
     cards: IIsCardMenuOpened[];
 }
 interface IIsCardMenuOpened{
@@ -18,16 +19,25 @@ interface IIsCardMenuOpened{
 
 export function Menu (props: IMenuProps) {
     function toggleCardDropdown(cardId:number){
-        if(!cardMenuOpened.cards[cardId]) cardMenuOpened.cards[cardId] = {cardId: cardId, isOpened: true}
-        else if(cardMenuOpened.cards[cardId].isOpened) cardMenuOpened.cards[cardId].isOpened = !cardMenuOpened.cards[cardId].isOpened;
-        setCardMenuOpened(cardMenuOpened);
+        const temp = Object.assign({}, cardMenuOpened);
+        //if(!cardMenuOpened.cards[cardId]) temp.cards[cardId] = {cardId: cardId, isOpened: true}
+        //else if(cardMenuOpened.cards[cardId]) temp.cards[cardId].isOpened = !temp.cards[cardId].isOpened;
+        if(temp.cards){
+            temp.cards.map((x)=>{
+                if(x.cardId === props.cardId) { console.log('props.cardId',props.cardId, x.isOpened);x.isOpened = !x.isOpened;}
+                else x.isOpened = false;
+            })
+        }
+            
+        setCardMenuOpened(temp);
+        console.log(111, cardMenuOpened);
     }
     function isCardMenuOpened(cardId:number){
+        console.log(222, cardMenuOpened);
         if(cardMenuOpened?.cards[cardId]?.isOpened) return true;
         return false;
     }
-    const [cardMenuOpened, setCardMenuOpened] = React.useState<ICardMenuOpened>({cards:[]});
-
+    const [cardMenuOpened, setCardMenuOpened] = React.useState<ICardMenuOpened>({cards:props.cards?.cards??[]});
     return (
         <div className={styles.menu} id={`menu${props.cardId}`}>
             <div className={styles.container}>
