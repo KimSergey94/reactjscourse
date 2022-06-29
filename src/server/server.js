2c050dd2885b0c83c644c556f5d78dd655c0eedc
KimSergey94 <sksss060213@gmail.com>
2022-06-29 14:40:02 +0600

    bug fix

--- src/server/server.js
+++ src/server/server.js
@@ -13,7 +13,7 @@ app.get('/auth', (req, res) => {
    console.log('auth code', req.query.code);
    axios.post(
       'https://www.reddit.com/api/v1/access_token',
-      `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.SERVER}/auth`,
+      `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.SERVER_URL}/auth`,
       {
          auth: { username: process.env.CLIENT_ID, password: process.env.CLIENT_PWD },
          headers: { 'Content-type': 'application/x-www-form-urlencoded'}
