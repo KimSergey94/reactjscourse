b5801c1b10ea1fff4c5da739fa36b6b9b812d74d
KimSergey94 <sksss060213@gmail.com>
2022-06-30 11:09:44 +0600

    NotFoundForm

--- src/shared/NotFoundForm/NotFoundForm.tsx
+++ src/shared/NotFoundForm/NotFoundForm.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './notfoundform.less';
 
 export function NotFoundForm() {
     return (
         <div className={styles.notFoundForm}>
             <span className={styles.notFoundFormMessage}>404 — страница не найдена</span>
+            <Link to="/">Вернуться на главную страницу</Link>
         </div>
+        
     );
 }
