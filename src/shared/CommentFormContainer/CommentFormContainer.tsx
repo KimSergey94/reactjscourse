c201f4f75014392c4aa7162f8de904d4ec48ddbf
KimSergey94 <sksss060213@gmail.com>
2022-07-05 15:30:43 +0600

    MobX на неиспользуемом компоненте

--- src/shared/CommentFormContainer/CommentFormContainer.tsx
+++ src/shared/CommentFormContainer/CommentFormContainer.tsx
@@ -1,26 +1,45 @@
+import { makeAutoObservable } from 'mobx';
+import { observer } from 'mobx-react-lite';
 import React, { ChangeEvent, FormEvent } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState, updateComment } from '../../store/store';
 import { CommentForm } from '../CommentForm/CommentForm';
 
-export function CommentFormContainer(){
-    const value = useSelector<RootState, string>(state => state.commentText);
-    const dispatch = useDispatch();
+
+export class Comment{
+    value = 'Привет из MobX';
+
+    constructor(){
+        makeAutoObservable(this);
+    }
+
+    updateValue(newValue:string){
+        this.value = newValue;
+    }
+}
+
+const myComment = new Comment();
+
+export const CommentFormContainer = observer(()=>{
+    // const value = useSelector<RootState, string>(state => state.commentText);
+    // const dispatch = useDispatch();
 
     function handleChange(event: ChangeEvent<HTMLTextAreaElement>){
-        dispatch(updateComment(event.target.value));
+        // dispatch(updateComment(event.target.value));
+        myComment.updateValue(event.target.value);
     }
 
     function handleSubmit(event: FormEvent) {
         event.preventDefault();
-        console.log(value);
+        console.log(myComment.value);
     }
+    
 
     return(
         <CommentForm
-            value={value}
+            value={myComment}
             onChange={handleChange}
             onSubmit={handleSubmit}
         />
     );
-}
+})
