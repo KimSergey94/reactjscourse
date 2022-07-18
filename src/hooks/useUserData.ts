79f8e1c3ea6d82726b5e368be55bf8f38008054b
KimSergey94 <sksss060213@gmail.com>
2022-07-18 11:04:33 +0600

    cleaning and formatting code

--- src/hooks/useUserData.ts
+++ src/hooks/useUserData.ts
@@ -1,29 +1,25 @@
-import axios from "axios";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { IUserData, meRequestAsync } from "../store/me/actions";
-import { RootState, setToken } from "../store/store";
-
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { authRequestAsync, IUserData } from '../store/auth/actions'
+import { RootState, setToken } from '../store/store'
 
 export function useUserData() {
-    // const [data, setData] = useState<IUserData>();
-    //const token = useContext(tokenContext);
-    const data = useSelector<RootState, IUserData>(state => state.me.data);
-    const loading = useSelector<RootState, boolean>(state => state.me.loading);
-    const token = useSelector<RootState, string>(state => state.token);
-    const dispatch = useDispatch();
+  const data = useSelector<RootState, IUserData>((state) => state.me.data)
+  const loading = useSelector<RootState, boolean>((state) => state.me.loading)
+  const token = useSelector<RootState, string>((state) => state.token)
+  const dispatch = useDispatch()
 
-    useEffect(() => {
-        if(window.__token__ && !token){
-            dispatch(setToken(window.__token__));
-        }
-        if(!token) return;
-
-        dispatch(meRequestAsync());
-    }, [token])
-
-    return {
-        data,
-        loading,
+  useEffect(() => {
+    if (window.__token__ && !token) {
+      dispatch(setToken(window.__token__))
     }
-} 
+    if (!token) return
+
+    dispatch(authRequestAsync())
+  }, [token])
+
+  return {
+    data,
+    loading,
+  }
+}
