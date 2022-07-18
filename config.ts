cdc520e19f4a32619c7252ab4f3e76f874088fb9
KimSergey94 <sksss0602123@gmail.com>
2022-07-18 22:36:18 +0600

    Content/Layout компонент

--- config.ts
+++ config.ts
@@ -1,3 +1,12 @@
-export const CLIENT_ID = process.env.NODE_ENV === 'production' ? 'EUHTrfLPfr0Qb4hU3F-NcA' : process.env.CLIENT_ID;
-export const CLIENT_PWD = process.env.NODE_ENV === 'production' ? 'V9mTS59TMyDO42K-PqCGKA5oSHlAJQ' : process.env.CLIENT_PWD;
-export const SERVER_URL = process.env.NODE_ENV === 'production' ? 'https://skillbox-react-app1.herokuapp.com' : process.env.SERVER_URL;
+export const CLIENT_ID =
+  process.env.NODE_ENV === 'production'
+    ? 'EUHTrfLPfr0Qb4hU3F-NcA'
+    : process.env.CLIENT_ID
+export const CLIENT_PWD =
+  process.env.NODE_ENV === 'production'
+    ? 'V9mTS59TMyDO42K-PqCGKA5oSHlAJQ'
+    : process.env.CLIENT_PWD
+export const SERVER_URL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://skillbox-react-app1.herokuapp.com'
+    : process.env.SERVER_URL
