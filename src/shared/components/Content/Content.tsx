eb41bafaab4eb85f88cf16d01f0d77c55674c8ab
KimSergey94 <sksss0602123@gmail.com>
2022-07-20 22:27:47 +0600

    postContent fix

--- src/shared/components/Content/Content.tsx
+++ src/shared/components/Content/Content.tsx
@@ -3,7 +3,12 @@ import styles from './content.less'
 
 interface IContentProps {
   children?: React.ReactNode
+  isPostContent?: boolean
 }
-export function Content({ children }: IContentProps) {
-  return <main className={styles.content}>{children}</main>
+export function Content({ children, isPostContent = false }: IContentProps) {
+  return (
+    <main className={isPostContent ? styles.postContent : styles.content}>
+      {children}
+    </main>
+  )
 }
